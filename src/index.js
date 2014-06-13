f710729b798835fad65dd96046e994ce42aaa37b
Leon Mergen <leon@solatis.com>
2014-06-13 10:07:30 +0700

    Initial checkin.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+var Queue = function () {
+
+}
+
+Queue.prototype.process = function (handler) {
+    if (this.handler) throw Error("Cannot define a handler more than once per Queue instance");
+
+    this.handler = handler;
+}
+
+Queue.prototype.add = function (data) {
+    job = this.createJob(data);
+
+    if (!this.handler) throw Error("Mocking version requires handler to be set before first add()");
+
+    this.handler(job);
+}
+
+Queue.prototype.createJob = function (data) {
+    return {
+        'data': data
+    }
+}
+
+module.exports = Queue;
---
+++ test/tests.spec.js
@@ -0,0 +1,40 @@
+var chai = require('chai');
+chai.use(require('chai-interface'));
+
+var Bull = require('..');
+
+describe('When setting a handler multiple times', function () {
+
+    it('Should throw an error', function () {
+
+        var queue = new Bull('myQueue');
+
+        queue.process(function (job) {});
+        chai.expect(queue.process.bind(queue)).to.throw(/Cannot define a handler more than once per Queue instance/);
+    });
+
+});
+
+describe('When adding an item without a handler', function () {
+
+    it('Should throw an error', function () {
+        var queue = new Bull('myQueue');
+        chai.expect(queue.add.bind(queue)).to.throw(/Mocking version requires handler to be set before first add/);
+    });
+});
+
+
+
+describe('When adding an item with a handler', function () {
+
+    it('Should be processed', function (done) {
+        var queue = new Bull('myQueue');
+
+        queue.process(function (job) {
+            chai.expect(job.data).to.equal('test');
+            done();
+        });
+
+        queue.add('test');
+    });
+});
